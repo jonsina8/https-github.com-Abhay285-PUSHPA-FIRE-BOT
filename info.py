b39152681f25aa706e165a63669637ab2d45be5c
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-02-26 21:57:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'public_file_channel_PF
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Ak9u-iW4Lps5MjFl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movies_updet')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mm_discussionn') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/MR_ABHAY_K')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Kingak70')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
