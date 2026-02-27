1c734c2fc92df361166cb70bc4a4a5f37b19165d
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-02-27 18:37:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mlapawar:mlapawar@clus
 DATABASE_NAME = environ.get('DATABASE_NAME', "mlapawar")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Abhay")
 
-MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
 O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
