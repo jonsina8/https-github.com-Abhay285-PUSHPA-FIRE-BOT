60866a0f9c77405a3f173021631c1c7f55076649
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-02-27 18:53:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mlapawar:mlapawar@cluster0.7sacruh.mongodb.net/?appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "mlapawar")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Abhay")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Abhay')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
